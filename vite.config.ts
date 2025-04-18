2ec5f6b8a9b4a171f9e0a15407434c8d2d88443e
wangJianHui <local_host@163.com>
2025-04-18 19:00:29 +0800

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
+
+// 获取仓库名称，用于GitHub Pages部署
+// 例如，如果您的GitHub仓库是 username/ops-platform，那么base就是'/ops-platform/'
+// 如果是在本地开发环境，则使用'/'
+const base = process.env.NODE_ENV === 'production' ? '/WBROMProductPrototype/' : '/'
 
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-    cors: true,
-  }
+  base: base, // 设置部署的基本路径
 })
